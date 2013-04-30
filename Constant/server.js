68e58930f135ad8762081fcced83a54e29c4318e
PereKastor <cbrunel@ubuntu.(none)>
2013-04-30 17:42:41 -0400

    authentification

--- Constant/server.js
+++ Constant/server.js
@@ -5,7 +5,8 @@ var http     = require('http'),
 	$ 	     = require('jquery'),
 	jq 	     = require('jquery').create(),
 	jQuery   = require('jquery').create(myWindow),
-	mysql    = require('mysql');
+	mysql    = require('mysql'),
+	url = require('url');
 
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -16,14 +17,6 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-connection.query('SELECT * FROM users WHERE id = 1', function(err, rows, fields) {
-  if (err) throw err;
-
-  console.log('Username : ', rows[0].username);
-  console.log('Password : ', rows[0].password);
-});
-
-connection.end();
 
 map = require('./js/Map');
 
@@ -32,8 +25,51 @@ var terrain = new map.Start();
 
 var test = terrain.largeur;
 
-var server = http.createServer(function (request, response) {
-	response.end();
+var server = http.createServer(function (req, res) {
+    // var page = url.parse(req.url).pathname;
+    // if (page == '/') {
+    // 	res.writeHead(200, {"Content-Type": "text/html"});
+    //     res.write(fs.readFileSync(__dirname + '/index.html'));
+    // }
+    // else if (page == '/login') {
+    // 	res.writeHead(200, {"Content-Type": "text/html"});
+    //     res.write(fs.readFileSync(__dirname + '/index.html'));
+    // }
+    // else if (page == '/logout') {
+    // 	res.writeHead(200, {"Content-Type": "text/html"});
+    //     res.write(fs.readFileSync(__dirname + '/index.html'));
+    // }
+    // else
+    // {
+    //     res.write('Bad way !!!');    	
+    // }
+    // res.end();
+
+
 });
 
 server.listen(1337);
+
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function(socket){
+
+	socket.on('login', function(datalogin){
+		connection.query('SELECT * FROM users WHERE username = "' + datalogin.username + '"', function(err, rows, fields) {
+			if (err) throw err;
+			if(rows.length > 0)
+			{
+				if(rows[0].password == datalogin.password)
+				{
+					io.sockets.emit('newuser', datalogin.username);
+				}
+				else
+					console.log('fail');
+			}
+			else
+				console.log('bad username/password');
+
+		});
+	});
+
+});
