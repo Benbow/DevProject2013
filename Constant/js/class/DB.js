623c3925cd2d325f2800a02d81036efbee78ea06
PereKastor <cbrunel@ubuntu.(none)>
2013-05-03 14:18:04 -0400

    creation DB.class

---
+++ Constant/js/class/DB.js
@@ -0,0 +1,30 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'toor',
+    database : 'farmDB',
+});
+
+var DB = (function() {
+
+    function DB(){ };
+
+    DB.prototype.insert = function(table,data,colum = "") {
+        connection.query('INSERT INTO ' + table + '(' + colum + ') VALUES (' + data + ');', function(err, rows, fields) {
+            if (err) throw err;
+            console.log('insert into' + table + ' done');
+        });
+    };
+
+    DB.prototype.setL = function(L) {
+        _longeur = L;
+    };
+
+    return DB;
+})();
+
+exports.DB = function(){
+	var a = new DB();
+	return a;
+}
