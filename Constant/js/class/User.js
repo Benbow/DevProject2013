4e5b2f118e574a2da70250e01bf9dd406ed9de4c
PereKastor <cbrunel@ubuntu.(none)>
2013-05-08 17:43:09 -0400

    Classe User

--- Constant/js/class/User.js
+++ Constant/js/class/User.js
@@ -1,35 +1,13 @@
-
-DB  = require('./DB.js');
-
-
-
+var DB = require('./DB');
 
 var User = (function() {
     var _id;
     var _pseudo;
+    var _DB = new DB();
 
     function User(id){
-        connection.query('SELECT * FROM Users WHERE id = ' + id, function(err, rows, fields) {
-            if (err) throw err;
-
-            this._id = id;
-            this._pseudo = rows[0].pseudo;
-        });
-    };
-
-
-   function User(){ };
-
-   User.prototype.getUser =  function (id)
-   {
-
-        var  db = new DB();
-
-        db.selectRequest("*","Users","WHERE id = " + id);
+        var connection = _DB.connection();
         
-
-            console.log(rows[0].pseudo);
-       
     };
 
     User.prototype.getPseudo = function(){
