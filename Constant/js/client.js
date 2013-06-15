ff308aeda76a6beae559f79e1c463180d4684a77
Benbow <126354@supinfo.com>
2013-06-15 11:54:40 -0400

    add new images arnaud

--- Constant/js/client.js
+++ Constant/js/client.js
@@ -237,7 +237,7 @@
 		});
 
 		$.each(map.maison, function(index, value) {
-	    	ppmap.addBuilding(value.x, value.y, 'images/house.png', 0, 0);
+	    	ppmap.addBuilding(value.x, value.y, 'images/house.png', 0, -64);
 		});
 
 		//Mise en place des batiments quand tu load la map.
@@ -1020,7 +1020,7 @@
 			User.isAttacking = false;
 			$(this).val('Conquerir !');
 			$(this).css('background','orange');
-			ppmap.changeCursor('images/attackTile.png','images/emptyTile.png',0,0);
+			ppmap.changeCursor('images/conquer.png','images/emptyTile.png',0,0);
 		}
 		
 	});
